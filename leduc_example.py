9b5772c8eb1989232ac91d336f0849d06b514fca
rmahajan14 <rm3601@columbia.edu>
2019-08-13 12:27:20 -0400

    add

--- leduc_example.py
+++ leduc_example.py
@@ -72,7 +72,7 @@ if __name__ == '__main__':
 
                                          lbr_args=LBRArgs(
                                              lbr_bet_set=bet_sets.B_5,
-                                             n_lbr_hands_per_seat=15000,
+                                             n_lbr_hands_per_seat=500,
                                              lbr_check_to_round=Poker.TURN,
                                              n_parallel_lbr_workers=N_LBR_WORKERS,
                                              use_gpu_for_batch_eval=False,
